87bae10ccf06ae77e38d6041aa9a3eac86a52a66
yang3wei <88480585@qq.com>
2013-02-02 11:44:59 +0800

    Add Bruce.java & Bruce.class

---
+++ Bruce.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Bruce {
+
+    public Bruce() {
+        System.out.println("Bruce init!");
+    }
+
+    public static void main(String args[]) {
+        Bruce b = new Bruce();
+        System.out.println("Hi, this is Bruce!");
+    }
+}
